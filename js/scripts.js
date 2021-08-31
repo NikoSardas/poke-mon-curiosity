759a3b2d9ad7f67837c0f6b3d365b8d4890311e7
nikosrds <89710667+nikosrds@users.noreply.github.com>
2021-08-31 14:47:02 -0400

    Pokémon display loop variable fix

--- js/scripts.js
+++ js/scripts.js
@@ -23,10 +23,10 @@ let pokemonList = [
     ]
   },
 ];
+const bigAlert = "Wow that's big!";
 for (let i = 0; i < pokemonList.length; i++) {
   const name = pokemonList[i].name;
   const height = pokemonList[i].height;
-  const bigAlert = "Wow that's big!";
   document.write(`${name} (height:${height})`);
   height > 1 && document.write(' ' + bigAlert);
   document.write('<br>')
