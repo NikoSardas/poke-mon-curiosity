fc4d634a7895c04962d679d1b5e193fc4110f5ff
nikosrds <89710667+nikosrds@users.noreply.github.com>
2021-09-01 14:10:56 -0400

    IIFE update Added pokemonRepository with public functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,76 @@
-let pokemonList = [
-  {
-    name: 'Genesect',
-    height: '1.5',
-    types: [
-      'bug',
-      'steel'
-    ]
-  },
-  {
-    name: 'Stunfisk',
-    height: '0.7',
-    types: [
-      'electric',
-      'ground'
-    ]
-  },
-  {
-    name: 'Chimchar',
-    height: '0.5',
-    types: [
-      'fire'
-    ]
-  },
-];
-let i = 0;
+let pokemonRepository = (function () {
+  let pokemonList = [
+    {
+      name: 'Genesect',
+      height: '1.5',
+      types: [
+        'bug',
+        'steel'
+      ]
+    },
+    {
+      name: 'Stunfisk',
+      height: '0.7',
+      types: [
+        'electric',
+        'ground'
+      ]
+    },
+    {
+      name: 'Chimchar',
+      height: '0.5',
+      types: [
+        'fire'
+      ]
+    },
+  ];
 
-//display pokemonList names and heights
-for (; i < pokemonList.length;) {
-  const height = pokemonList[i].height;
-  let displayText = pokemonList[i].name + ' (height: ' + height + ')';
-  //check for adding big text:
+  function getAll() {
+    return pokemonList;
+  }
+
+  function add(pokemon) {
+    function checkPokemonKeys() {
+      const pokemonKeys = Object.keys(pokemon);
+      // return keyTest boolean
+      return (pokemonKeys[0] === "name" && pokemonKeys[1] === "height" && pokemonKeys[2] === "type");
+    }
+
+    // return pokemon if tests are validated
+    typeof pokemon === 'object' && checkPokemonKeys() && pokemonList.push(pokemon);
+  }
+
+
+  function findPokemon(name) {
+    function checkName(pokemon) {
+      if (pokemon.name === name) {
+        return pokemon;
+      }
+    }
+
+    let filterResult = pokemonList.filter(checkName);
+    if (filterResult.length === 0) {
+      filterResult = 'That Pokemon is not on the list!'
+    }
+    return filterResult;
+  }
+
+  return {
+    getAll: getAll,
+    add: add,
+    find: findPokemon
+  }
+})()
+
+function printPokemonDetails(pokemon) {
+  const height = pokemon.height;
+  let displayText = pokemon.name + ' (height: ' + height + ')';
   height > 1 && (displayText = displayText + (' ' + "Wow that's big!"));
   document.write(displayText, '<br>');
-  i++;
 }
+
+pokemonRepository.getAll().forEach(printPokemonDetails);
+
+
+console.log(pokemonRepository.find('Stunfisk'));
+console.log(pokemonRepository.find('Stunkfisk'));
